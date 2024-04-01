94c57a85d12e0b09dc9d7a57c88d55a5d6ca4e7e
merricat <zkerr4032@outlook.com>
2024-04-01 19:46:44 +0100

    Add ball movement and basic collision

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 #define SCREEN_WIDTH  500
 #define SCREEN_HEIGHT 500
@@ -7,7 +8,15 @@
 #define PADDLE_HEIGHT 75
 #define PADDLE_WIDTH  15
 
+#define BALL_SPEED 1
+
+
 bool isCursorOnScreen(int pos);
+
+typedef enum {
+  LEFT,
+  RIGHT
+} Direction;
 
 int main() {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PONG");
@@ -18,20 +27,37 @@ int main() {
   Rectangle p2 = {15, 200, PADDLE_WIDTH, PADDLE_HEIGHT};
   Rectangle ball = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 15, 15};
   HideCursor();
+  int gradient = 0;
+  Direction dir = LEFT;
   
-
+  
   while(!WindowShouldClose()) {
     int cursorY = GetMouseY();
     if (isCursorOnScreen(cursorY)){
       p1.y = cursorY - (PADDLE_HEIGHT / 2); 
     }
-    
+    switch (dir) {
+    case LEFT:
+      ball.x = ball.x + BALL_SPEED;
+      break;
+    case RIGHT:
+      ball.x = ball.x - BALL_SPEED;
+      break;
+    }
+    ball.y += gradient;
+
+    if (CheckCollisionRecs(ball, p1)) {
+      dir = RIGHT;
+    }
+    if (CheckCollisionRecs(ball, p2)) {
+      dir = LEFT;
+    }
+
     BeginDrawing();
-    DrawRectangleLinesEx(border, 5.0, WHITE);
-    DrawRectangleRec(p1, WHITE);
-    DrawRectangleRec(p2, WHITE);
-    DrawRectangleRec(ball, WHITE);
-    
+        DrawRectangleLinesEx(border, 5.0, WHITE);
+        DrawRectangleRec(p1, WHITE);
+        DrawRectangleRec(p2, WHITE);
+        DrawRectangleRec(ball, WHITE); 
     ClearBackground(BLACK);
     EndDrawing();
   }
